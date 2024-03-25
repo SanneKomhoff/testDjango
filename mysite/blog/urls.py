4ef4a40f2afa2ea9b1ec13ab474e2625fa73da25
SanneKomhoff <sannekomhoff@gmail.com>
2024-03-25 15:45:13 +0000

    Wijzigingen mbt les 6, toevoegen poll.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_inhoud'),
+    path('post/nieuw/', views.post_nieuw, name='post_nieuw'),
+    path('delete/<post_id>',views.delete_post,name='delete')
 ]
 
